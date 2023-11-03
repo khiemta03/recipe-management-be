de062311951267865c49d85728a2cb86e666b7bf
21120480 <tndkhiemqn2k3@gmail.com>
2023-11-03 13:12:45 +0700

    add cors accepts all origins

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const { authRoutes, recipeCategoryRoutes, recipeRoutes } = require('./src/routes/index')
-
+const cors = require('cors')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+  }
+app.use(cors(corsOptions))
 
 //routes
 app.use('/api/v1/auth', authRoutes)
