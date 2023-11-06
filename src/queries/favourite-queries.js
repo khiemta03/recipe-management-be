64528825794422fc28d94a6a4af25b1aa826c73c
21120480 <tndkhiemqn2k3@gmail.com>
2023-11-06 21:15:43 +0700

    fix: change '' to []

--- src/queries/favourite-queries.js
+++ src/queries/favourite-queries.js
@@ -4,14 +4,13 @@ const postgres = require('../databases/postgreSQL')
 
 const getFavouriteRecipes = async (userId) => {
     const queryString = `select RecipeId from FAVORITE where UserId = '${userId}'`;
-
+    
     try {
         const recipeData = await postgres.query(queryString);
         if(recipeData.rowCount > 0) {
-            // console.log(recipeData)
             return recipeData.rows;
         }
-        return '';
+        return [];
     }
     catch(err) {
         throw err;
