7c395c6ded1ca293e63c9bfc11f2bc5a2ff0f4bb
21120480 <tndkhiemqn2k3@gmail.com>
2023-11-07 18:38:38 +0700

    fix: sql injection by using a parameterized query

--- src/queries/role-queries.js
+++ src/queries/role-queries.js
@@ -2,17 +2,16 @@ const postgres = require('../databases/postgreSQL')
 
 
 const getRoleByRoleId = async (roleId) => {
-    let queryString = `select name from roles where roleid = '${roleId}'`;
-
+    const queryString = 'select name from roles where roleid = $1';
+    const values = [roleId]
     try {
-        const roleQueryData = await postgres.query(queryString);
-        if(roleQueryData.rowCount > 0) {
-            return roleQueryData.rows[0].name;
-        }
-        return '';
+        const roleQueryData = await postgres.query(queryString, values);
+        const formattedData = roleQueryData.rowCount > 0 ? roleQueryData.rows[0].name : ''
+
+        return formattedData
     }
-    catch(err) {
-        throw err;
+    catch (err) {
+        throw new Error('Internal Server Error')
     }
 }
 
--- src/queries/user-queries.js
+++ src/queries/user-queries.js
@@ -1,32 +1,32 @@
+const e = require('cors')
 const postgres = require('../databases/postgreSQL')
 
 
 //Dung id hoac username
 const getUserProfile = async (obj) => {
-    let queryString = ''
-
-    queryString = obj.username ? `select * from Users where Username = '${obj.username}'` : `select * from Users where UserId = '${obj.id}'`
+    const queryString = obj.username ? 'select * from Users where Username = $1;' : 'select * from Users where UserId = $1;'
+    const values = obj.username ? [obj.username] : [obj.userId]
     try {
-        const userQueryData = await postgres.query(queryString)
+        const userQueryData = await postgres.query(queryString, values)
 
         const userFormattedData = userQueryData.rowCount >= 1 ? userQueryData.rows[0] : {}
         return userFormattedData
     }
     catch (err) {
-        throw (err)
+        throw new Error('Internal Server Error')
     }
 }
 
 
 //add new user for register function
 const addNewUser = async (username, password, name, email, role = 1, avatar = null) => {
-    let queryString = `insert into Users(username, password, name, email, role, avatar)
-                        values('${username}', '${password}', '${name}', '${email}', ${role}, '${avatar}')`;
+    const queryString = 'insert into Users(username, password, name, email, role, avatar) values($1, $2, $3, $4, $5, $6)';
+    const values = [username, password, name, email, role, avatar]
     try {
-        await postgres.query(queryString);
+        await postgres.query(queryString, values);
     }
     catch (err) {
-        throw err
+        throw new Error('Internal Server Error')
     }
 }
 
