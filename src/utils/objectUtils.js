673078ec4d215980d3455e37ce457f260d2d92c4
21120480 <tndkhiemqn2k3@gmail.com>
2023-11-08 18:20:33 +0700

    update new objectUtils.empty function

--- src/utils/objectUtils.js
+++ src/utils/objectUtils.js
@@ -1,12 +1,21 @@
-const isEmpty = (obj = {}) => {
-    for (const prop in obj) {
-        if (Object.hasOwn(obj, prop)) {
-            return false;
-        }
-    }
+// const isEmpty = (obj = {}) => {
+//     for (const prop in obj) {
+//         if (Object.hasOwn(obj, prop)) {
+//             return false;
+//         }
+//     }
 
-    return true;
-}
+//     return true;
+// }
 
+const isEmpty = (obj) => {
+    if (obj === null || obj === undefined) return true;
+
+    const values = Object.values(obj);
+    return (
+        (Object.keys(obj).length === 0 && obj.constructor === Object) ||
+        values.every((x) => x === null || x === undefined)
+    );
+};
 
 module.exports = { isEmpty }
