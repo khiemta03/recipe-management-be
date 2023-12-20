cdc8192bd88e7e3bb0b8270e97086ab05886dca1
21120470 <nhathuynguyen217@gmail.com>
2023-12-20 23:17:55 +0700

    add ratingRoutes into index.js, validate data in favourite controller and rating controller

--- src/controllers/rating-controller.js
+++ src/controllers/rating-controller.js
@@ -1,11 +1,15 @@
 const { Rating, getNumberRatingOfRecipe, getUserRatingofRecipe, updateRating, getRatingofUser } = require('../queries/rating-queries.js');
+const boolean = require('../utils/booleanUtils');
 
 //post
 const ratingController = async (req, res) => {
-    const userId = req.user.userId;
-    const recipeId = req.body['recipe'];
-    const rate = req.body['rating'];
+    let userId = req.user.userId;
+    let recipeId = req.body['recipe'];
+    let rate = req.body['rating'];
     try {
+        userId = boolean.uuidValidate(userId);
+        recipeId = boolean.uuidValidate(recipeId);
+        rate = boolean.integerValidate(rate);
         await Rating(userId, recipeId, rate);
         res.json({
             status: 200,
@@ -21,9 +25,11 @@ const ratingController = async (req, res) => {
 
 //get
 const getRatingOfRecipeController = async (req, res) => {
-    const userId = req.user.userId;
-    const recipeId = req.query['recipe'];
+    let userId = req.user.userId;
+    let recipeId = req.query['recipe'];
     try {
+        userId = boolean.uuidValidate(userId);
+        recipeId = boolean.uuidValidate(recipeId);
         const userCount = await getNumberRatingOfRecipe(recipeId);
         const userData = await getUserRatingofRecipe(recipeId);
         const rated = await getRatingofUser(userId, recipeId);
@@ -43,10 +49,13 @@ const getRatingOfRecipeController = async (req, res) => {
 
 //put
 const updateRatingController = async (req, res) => {
-    const userId = req.user.userId;
-    const recipeId = req.params['id'];
-    const newRate = req.body['newrate'];
+    let userId = req.user.userId;
+    let recipeId = req.params['id'];
+    let newRate = req.body['newrate'];
     try {
+        userId = boolean.uuidValidate(userId);
+        recipeId = boolean.uuidValidate(recipeId);
+        newRate = boolean.integerValidate(newRate);
         await updateRating(userId, recipeId, newRate);
         res.json({
             status: 200,
