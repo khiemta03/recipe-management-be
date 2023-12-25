807638c9360346db8bac1091f7cf0be5c4d65fa1
21120480 <tndkhiemqn2k3@gmail.com>
2023-12-25 11:13:15 +0700

    add: role api can return super-admin

--- src/controllers/role-controller.js
+++ src/controllers/role-controller.js
@@ -4,7 +4,6 @@ const { getAllRoles } = require('../queries/index')
 const getRolesController = async (req, res) => {
     try {
         const roleQueryData = await getAllRoles()
-        const roleData = roleQueryData.filter(role => role.name !== 'SuperAdmin')
         res.json({
             status: 200,
             data: roleData
